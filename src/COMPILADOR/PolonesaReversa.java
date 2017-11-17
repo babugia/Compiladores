52fd56ea0c5100f27eed9137083adc4cc0eee95b
guilherme babugia <babugia@icloud.com>
2017-11-17 20:57:04 -0200

    posfix quase la

--- src/COMPILADOR/PolonesaReversa.java
+++ src/COMPILADOR/PolonesaReversa.java
@@ -1,74 +1,165 @@
 package src.COMPILADOR;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class PolonesaReversa {
-	
-	private String infixa;
-	
-	public String getInfixa()
-	{
-		return infixa; //notação normal, que o ser humano faz
+
+	private ArrayList<String> infixa;
+
+	public ArrayList<String> getInfixa() {
+		return infixa; // notaï¿½ï¿½o normal, que o ser humano faz
 	}
-   public void setInfixa(String infixa) //regras de negocio para a infixa
-   {
-        this.infixa = infixa;
-   }
-   
-   public String toPolonesaReversaDesempilha() {
-       Pilha p = new Pilha();
-       String saida = "";
-       for (int i = 0; i < infixa.length(); i++) {
-           char ch = infixa.charAt(i);
-           if (Character.isDigit(ch)) {
-               saida += ch;
-           }
-           
-           if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-        	   
-        	   if (!p.isEmpty() && prioridadeOperandos(p.top().toString().charAt(0)) >= prioridadeOperandos(ch)) {
-        		   p.push(ch);
-        	   }else {
-        		   
-        		   while (!p.isEmpty() && prioridadeOperandos(p.top().toString().charAt(0)) >= prioridadeOperandos(ch)) {
-                       saida += p.pop();
-                   }
-        	   }
-               p.push(ch);
-           }
-           if (ch == '(') {
-               p.push(ch);
-           }
-           if(ch == ')') {
-               while(p.top().toString().charAt(0) != '(') {
-                   saida += p.pop();
-               }
-               p.pop();
-           }
-       }
-       while(!p.isEmpty()) {
-           saida += p.pop();
-       }
-       return saida;
-   }
-   
-   private int prioridadeOperandos(char opcao) {
-       int resp = 0;
-       switch (opcao) {
-           case '(':
-               resp = 1; //parenteses tem a maior prioridade
-               break;
-           case '+':
-               resp = 2; //o + tem a terceira maior prioridade
-               break;
-           case '-':
-               resp = 2; //o - tem a terceira maior prioridade
-               break;
-           case '*':
-               resp = 3; //multiplicação tem a segunda maior prioridade
-               break;
-           case '/':
-               resp = 3; //ou divisão tem a segunda maior prioridade
-               break;
-       }
-       return resp;
-   }
+
+	public void setInfixa(ArrayList<String> infixa) // regras de negocio para a infixa
+	{
+		this.infixa = infixa;
+	}
+
+	public ArrayList<String> toPolonesaReversaDesempilha() {
+		Pilha p = new Pilha();
+		Stack<String> stack = new Stack<String>();
+		System.out.println(getInfixa());
+
+		ArrayList<String> saida = new ArrayList<String>();
+		// String saida = "";
+		for (int i = 0; i < infixa.size(); i++) {
+			String ch = infixa.get(i);
+			if (pode(ch)) {
+				// saida += ch;
+				saida.add(ch);
+
+			}
+
+			if (ch == "+" || ch == "-" || ch == "*" || ch == "div" || ch == ">"
+					|| ch == "<" || ch == ">=" || ch == "<=" || ch == "!=" 
+					|| ch == "=" || ch == "e" || ch == "ou" || ch == "-u" 
+					|| ch == "+u" || ch == "nao") {
+
+				if (!stack.isEmpty() &&  prioridadeOperandos(ch) > prioridadeOperandos(stack.peek())) {
+					//stack.push(ch);
+				} else {
+
+					while (!stack.isEmpty() && prioridadeOperandos(stack.peek()) >= prioridadeOperandos(ch)) {
+						// saida += stack.pop();
+						saida.add(stack.pop());
+					}
+				}
+				stack.push(ch);
+			}
+			if (ch == "(") {
+				stack.push(ch);
+			}
+			if (ch == ")") {
+				while (stack.peek() != "(") {
+					// saida += stack.pop();
+					saida.add(stack.pop());
+				}
+				stack.pop();
+			}
+		}
+		while (!stack.isEmpty()) {
+			// saida += stack.pop();
+			saida.add(stack.pop());
+		}
+		return saida;
+	}
+
+	/*
+	 * Prioridade +,0,nao *,/ =,- <,>,!=... e ou
+	 */
+
+	// n pode ser char por causa dos operadores >=, !=, -u, ....
+	private int prioridadeOperandos(String opcao) {
+		int resp = 0;
+		switch (opcao) {
+		case "(":
+			resp = 1; // parenteses tem a maior prioridade
+			break;
+		case "+":
+			resp = 5; // o + tem a terceira maior prioridade
+			break;
+		case "-":
+			resp = 5; // o - tem a terceira maior prioridade
+			break;
+		case "div":
+			resp = 6; // multiplicaï¿½ï¿½o tem a segunda maior prioridade
+			break;
+		case "*":
+			resp = 6; // ou divisï¿½o tem a segunda maior prioridade
+			break;
+		case "-u":
+			resp = 7;
+			break;
+		case "+u":
+			resp = 7;
+			break;
+		case ">":
+			resp = 4;
+			break;
+		case "<":
+			resp = 4;
+			break;
+		case ">=":
+			resp = 4;
+			break;
+		case "<=":
+			resp = 4;
+			break;
+		case "=":
+			resp = 4;
+			break;
+		case "!=":
+			resp = 4;
+			break;
+		case "e":
+			resp = 3;
+			break;
+		case "ou":
+			resp = 2;
+			break;
+
+		}
+		return resp;
+	}
+
+	public boolean ehInteiro(String s) {
+		// cria um array de char
+		char[] c = s.toCharArray();
+		boolean d = true;
+		for (int i = 0; i < c.length; i++) {
+			// verifica se o char nÃ£o Ã© um dÃ­gito
+			if (!Character.isDigit(c[i])) {
+				d = false;
+				break;
+			}
+		}
+		return d;
+	}
+
+	public boolean pode(String s) {
+		// cria um array de char
+		char[] c = s.toCharArray();
+		boolean d = true;
+		
+		if (!Character.isDigit(c[0]) && !Character.isLetter(c[0])) {
+			d = false;
+		}
+		//VAI DAR BOSTA SE FOR NAO, OU OU E
+		
+		if(c.length == 3) {
+			if(c[0] == 'n')
+				if(c[1] == 'a')
+					if(c[2] == 'o')
+						d = false;			
+		}else if(c.length == 2) {
+			if(c[0] == 'o')
+				if(c[1] == 'u')
+					d = false;
+		}else if(c.length == 1) {
+			if(c[0] == 'e')
+				d = false;
+		}
+		return d;
+	}
 }
--- src/COMPILADOR/testePolonesaReversa.java
+++ src/COMPILADOR/testePolonesaReversa.java
@@ -1,12 +1,47 @@
 package src.COMPILADOR;
 
+import java.util.ArrayList;
+
 public class testePolonesaReversa {
 	public static void main(String [] args)
     {
+		ArrayList<String> teste = new ArrayList<String>();
+//		teste.add("a");
+//		teste.add(">");
+//		teste.add( "b");
+//		teste.add( "e");
+//		teste.add( "5");
+//		teste.add( "=");
+//		teste.add( "c");
+		
+//		teste.add("4");
+//		teste.add("+");
+//		teste.add( "5");
+//		teste.add( "*");
+//		teste.add( "3");
+		
+//		teste.add("(");
+//		teste.add("4");
+//		teste.add("+");
+//		teste.add( "5");
+//		teste.add(")");
+//		teste.add( "*");
+//		teste.add( "3");
+		
+//		teste.add("-u");
+//		teste.add("a");
+//		teste.add("+");
+//		teste.add( "5");  			ESSE TA ESTRANHO
+//		teste.add( "*");
+//		teste.add( "+u");
+//		teste.add( "v");
+		
+		
 		PolonesaReversa expressaoo = new PolonesaReversa();   
-        expressaoo.setInfixa("1+2"); 
-        String expressao = expressaoo.toPolonesaReversaDesempilha();
-        System.out.println(expressao);
+        expressaoo.setInfixa(teste); 
+        ArrayList<String> expressa = new ArrayList<String>();
+        expressa = expressaoo.toPolonesaReversaDesempilha();
+        System.out.println(expressa);
     }  
 }
 
